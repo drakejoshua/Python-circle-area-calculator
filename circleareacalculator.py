c9eaf048af3147bc678e5d254daf2ac562d666af
drakejoshua <drakejoshua086@gmail.com>
2023-02-25 17:20:55 +0100

    Create circleareacalculator.py
    added the init files for this repo. this file would be the main file

---
+++ circleareacalculator.py
@@ -0,0 +1,8 @@
+print("This is a program designed by mabawonku joshua")
+print("This program is to be used to calculate the area of a circle")
+
+radius = input("Enter the radius of the circle you want to calculate( in cm )")
+
+result = 22 / 7 * float(radius) ** 2
+
+print("The area of the circle is", int(result), "square centimeters")
